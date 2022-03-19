2b266ab663d6e9109bd5538e9eaad0d7669a117e
Hossam <hossamegyqandel@gmail.com>
2022-03-19 22:08:40 +0200

    New PrintLine

--- src/MainPlayGround.java
+++ src/MainPlayGround.java
@@ -1,5 +1,10 @@
+
+
 public class MainPlayGround {
+
     public static void main(String[] args) {
         System.out.println("Hello From Kotlin Project!");
+        System.out.println("Hello Java Language");
     }
+
 }
