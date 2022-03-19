8015b9a2bfe75cc08280b4e752404f794cd8a537
Ibrahim Qandeel <iqandeel.c@stc.com.sa>
2022-03-19 23:21:03 +0300

    Change Greeting Message2

--- src/MainPlayGround.java
+++ src/MainPlayGround.java
@@ -5,6 +5,7 @@ public class MainPlayGround {
     public static void main(String[] args) {
         System.out.println("Hello From Kotlin Project!");
         System.out.println("Hello Java Language");
+        System.out.println("Hello Lara!");
     }
 
 }
